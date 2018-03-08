448629ba6879980db3fd2e5c7decaa76be715ccf
Vincent Chavez <vchavez92780@gmail.com>
2018-03-08 12:19:30 -0500

    updated tests

--- tests/string.js
+++ tests/string.js
@@ -7,5 +7,15 @@ describe('string.js', () => {
       expect(ucfirst('vince chavez')).to.equal('Vince chavez');
       done();
     });
+
+    it('should turn first letter to upper case', (done) => {
+      expect(ucfirst('Devin encalada')).to.equal('Devin encalada');
+      done();
+    });
+
+    it('should turn first letter to upper case', (done) => {
+      expect(ucfirst('DEVIN')).to.equal('DEVIN');
+      done();
+    });
   });
 });
