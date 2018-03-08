199e1d0058e0c90cbedf0a563290dc7300ac8a7b
Vincent Chavez <vchavez92780@gmail.com>
2018-03-08 17:01:56 -0500

    Updated string tests

--- tests/string.js
+++ tests/string.js
@@ -3,19 +3,11 @@ import { ucfirst } from '../src/string';
 
 describe('string.js', () => {
   describe('#ucfirst', () => {
-    it('should turn first letter to upper case', (done) => {
-      expect(ucfirst('vince chavez')).to.equal('Vince chavez');
-      done();
-    });
-
-    it('should turn first letter to upper case', (done) => {
-      expect(ucfirst('Devin encalada')).to.equal('Devin encalada');
-      done();
-    });
-
-    it('should turn first letter to upper case', (done) => {
-      expect(ucfirst('DEVIN')).to.equal('DEVIN');
-      done();
+    it('should turn first letter to upper case', () => {
+      expect(ucfirst('john doe')).to.equal('John doe');
+      expect(ucfirst('Jane Doe')).to.equal('Jane Doe');
+      expect(ucfirst(('JOHN').toLowerCase())).to.equal('John');
+      expect(ucfirst('John')).to.equal('John');
     });
   });
 });
