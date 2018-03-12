420e5dd33d313fedf1ef429acce7a1b2ef93d976
Vincent Chavez <vchavez92780@gmail.com>
2018-03-11 21:11:10 -0400

    Fixed date format

--- src/date.js
+++ src/date.js
@@ -3,12 +3,13 @@ import moment from 'moment';
 /**
  * Checks the specified date is equal to today or after.
  * @param {String} date
+ * @param {String} dateFormat
  * @returns {boolean}
  */
-export const isAfterToday = (date) => {
+export const isAfterToday = (date, dateFormat = 'YYYY-MM-DD') => {
   let valid = false;
   if (date) {
-    const startDate = moment(date);
+    const startDate = moment(date, dateFormat);
     if (startDate.diff(moment(), 'days') >= 0) {
       valid = true;
     }
